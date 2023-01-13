eeea3525f3a68c8fcfd42d49759529b4d64d26b6
Paul <pauloladapo13@gmail.com>
2023-01-13 10:34:56 +0000

    First commit

---
+++ CompSciNTUni/Session1.cpp
@@ -0,0 +1,19 @@
+// Session1.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 0;
+	double y = 42.5;
+	bool Answer = true;
+
+	cout << "The value of x is " << x << endl;
+	cout << "This is a section of text" << " And this is another" << endl;
+	cout << "The value of y is " << y << endl;
+	cout << "The value of Answer is " << Answer << endl;
+
+}
+
